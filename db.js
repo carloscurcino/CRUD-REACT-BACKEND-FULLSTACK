01a28fab94f2b2e46c84914b03948463cfa41a93
carloscurcino <carlosdanielf541@gmail.com>
2023-06-24 19:10:12 -0300

    test 2

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { createPool } = require('mysql2/promise');
 require('dotenv').config();
 
-const db = createPool({
+export const db = createPool({
     host: process.env.MYSQLHOST,
     port: process.env.MYSQLPORT,
     user: process.env.MYSQLUSER,
