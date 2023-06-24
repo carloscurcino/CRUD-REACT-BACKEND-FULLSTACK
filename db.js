4b90e540a6d4b49f9b8443d8b4d37c0f90678285
carloscurcino <carlosdanielf541@gmail.com>
2023-06-24 16:37:42 -0300

    conexão db atualizada

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: "us-cdbr-east-06.cleardb.net",
-    user: "baf3d64cf0062a",
-    password: "830f7b0d",
-    database: "heroku_3448442858d4e5b"
+    host: "containers-us-west-166.railway.app",
+    user: "root",
+    password: "7ryL8DSHWfwwSWedyIWc",
+    database: "railway"
 });
 
